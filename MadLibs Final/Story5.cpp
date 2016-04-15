2b8b48e09837803fc042071bc25f5b09cc69be82
makargue <mnargue02>
2016-04-15 08:28:14 -0400

    copy and pasted the outline for story5

---
+++ MadLibs Final/Story5.cpp
@@ -0,0 +1,171 @@
+#include "Story4.h"
+/*string adjective9;
+string adjective10;
+string adjective11;
+string pluralNoun10;
+string pluralNoun11;
+string pluralNoun12;
+string noun8;
+string noun9;
+string noun10;
+string noun11;
+string name1;
+string name2;
+string name3;
+string state1;
+string state2;
+string plant1;
+string story4;*/
+void Words4::setAdjective9(string value) {
+	adjective9 = value;
+}
+
+void Words4::setAdjective10(string value) {
+	adjective10 = value;
+}
+
+void Words4::setAdjective11(string value) {
+	adjective11 = value;
+}
+
+void Words4::setPluralNoun10(string value) {
+	pluralNoun10 = value;
+}
+
+void Words4::setPluralNoun11(string value) {
+	pluralNoun11 = value;
+}
+
+void Words4::setPluralNoun12(string value) {
+	pluralNoun12 = value;
+}
+
+void Words4::setNoun8(string value) {
+	noun8 = value;
+}
+
+void Words4::setNoun9(string value) {
+	noun9 = value;
+}
+
+void Words4::setNoun10(string value) {
+	noun10 = value;
+}
+
+void Words4::setNoun11(string value) {
+	noun11 = value;
+}
+
+void Words4::setName1(string value){
+	name1 = value;
+}
+
+void Words4::setName2(string value){
+	name2 = value;
+}
+
+void Words4::setName3(string value){
+	name3 = value;
+}
+
+void Words4::setState1(string value){
+	state1 = value;
+}
+
+void Words4::setState2(string value){
+	state2 = value;
+}
+
+void Words4::setPlant1(string value){
+	plant1 = value;
+}
+
+void Words4::setStory4(string value) {
+	story4 = value;
+}
+
+/*string adjective9;
+string adjective10;
+string adjective11;
+string pluralNoun10;
+string pluralNoun11;
+string pluralNoun12;
+string noun8;
+string noun9;
+string noun10;
+string noun11;
+string name1;
+string name2;
+string name3;
+string state1;
+string state2;
+string plant1;
+string story4;*/
+
+string Words4::getAdjective9() {
+	return adjective9;
+}
+
+string Words4::getAdjective10() {
+	return adjective10;
+}
+
+string Words4::getAdjective11() {
+	return adjective11;
+}
+
+string Words4::getPluralNoun10() {
+	return pluralNoun10;
+}
+
+string Words4::getPluralNoun11() {
+	return noun11;
+}
+
+string Words4::getPluralNoun12() {
+	return pluralNoun12;
+}
+
+string Words4::getNoun8() {
+	return noun8;
+}
+
+string Words4::getNoun9() {
+	return noun9;
+}
+
+string Words4::getNoun10(){
+	return noun10;
+}
+
+string Words4::getNoun11(){
+	return noun11;
+}
+
+string Words4::getName1(){
+	return name1;
+}
+
+string Words4::getName2(){
+	return name2;
+}
+
+string Words4::getName3(){
+	return name3;
+}
+
+string Words4::getState1(){
+	return state1;
+}
+
+string Words4::getState2(){
+	return state2;
+}
+
+string Words4::getPlant1(){
+	return plant1;
+}
+
+string Words4::getStory4(){
+	return story4;
+}
---
+++ MadLibs Final/Story5.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Words4{
+private:
+	string adjective9;
+	string adjective10;
+	string adjective11;
+	string pluralNoun10;
+	string pluralNoun11;
+	string pluralNoun12;
+	string noun8;
+	string noun9;
+	string noun10;
+	string noun11;
+	string name1;
+	string name2;
+	string name3;
+	string state1;
+	string state2;
+	string plant1;
+	string story4;
+public:
+	Words4();
+	void setAdjective9(string value);
+	void setAdjective10(string value);
+	void setAdjective11(string value);
+	void setPluralNoun10(string value);
+	void setPluralNoun11(string value);
+	void setPluralNoun12(string value);
+	void setNoun8(string  value);
+	void setNoun9(string value);
+	void setNoun10(string value);
+	void setNoun11(string value);
+	void setName1(string value);
+	void setName2(string Value);
+	void setName3(string Value);
+	void setState1(string Value);
+	void setState2(string Value);
+	void setPlant1(string Value);
+	void setStory4(string value);
+	string getAdjective9();
+	string getAdjective10();
+	string getAdjective11();
+	string getPluralNoun10();
+	string getPluralNoun11();
+	string getPluralNoun12();
+	string getNoun8();
+	string getNoun9();
+	string getNoun10();
+	string getNoun11();
+	string getName1();
+	string getName2();
+	string getName3;
+	string getState1;
+	string getState2;
+	string getPlant1;
+	string getStory4();
+};

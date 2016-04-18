3bf50191cc5fde17b3cbc5625bea7bdf7c8c5eea
makargue <mnargue02>
2016-04-18 07:52:44 -0400

    fixed void in story5

--- MadLibs Final/Story5.cpp
+++ MadLibs Final/Story5.cpp
@@ -1,87 +1,87 @@
-#include "Story4.h"
-/*string adjective9;
-string adjective10;
-string adjective11;
-string pluralNoun10;
-string pluralNoun11;
-string pluralNoun12;
-string noun8;
-string noun9;
-string noun10;
-string noun11;
-string name1;
-string name2;
-string name3;
-string state1;
-string state2;
-string plant1;
-string story4;*/
-void Words4::setAdjective9(string value) {
-	adjective9 = value;
+#include "Story5.h"
+/*  string noun12;
+	string noun13;
+	string noun14;
+	string noun15;
+	string noun16;
+	string adjective12;
+	string adjective13;
+	string verb2;
+	string verbING3;
+	string name4;
+	string animal1;
+	string n;
+	string name3;
+	string state1;
+	string state2;
+	string plant1;
+	string story4;*/
+void Words5::setNoun12(string value) {
+	noun12 = value;
 }
 
-void Words4::setAdjective10(string value) {
+void Words5::setNoun13(string value) {
 	adjective10 = value;
 }
 
-void Words4::setAdjective11(string value) {
+void Words5::setAdjective11(string value) {
 	adjective11 = value;
 }
 
-void Words4::setPluralNoun10(string value) {
+void Words5::setPluralNoun10(string value) {
 	pluralNoun10 = value;
 }
 
-void Words4::setPluralNoun11(string value) {
+void Words5::setPluralNoun11(string value) {
 	pluralNoun11 = value;
 }
 
-void Words4::setPluralNoun12(string value) {
+void Words5::setPluralNoun12(string value) {
 	pluralNoun12 = value;
 }
 
-void Words4::setNoun8(string value) {
+void Words5::setNoun8(string value) {
 	noun8 = value;
 }
 
-void Words4::setNoun9(string value) {
+void Words5::setNoun9(string value) {
 	noun9 = value;
 }
 
-void Words4::setNoun10(string value) {
+void Words5::setNoun10(string value) {
 	noun10 = value;
 }
 
-void Words4::setNoun11(string value) {
+void Words5::setNoun11(string value) {
 	noun11 = value;
 }
 
-void Words4::setName1(string value){
+void Words5::setName1(string value){
 	name1 = value;
 }
 
-void Words4::setName2(string value){
+void Words5::setName2(string value){
 	name2 = value;
 }
 
-void Words4::setName3(string value){
+void Words5::setName3(string value){
 	name3 = value;
 }
 
-void Words4::setState1(string value){
+void Words5::setState1(string value){
 	state1 = value;
 }
 
-void Words4::setState2(string value){
+void Words5::setState2(string value){
 	state2 = value;
 }
 
-void Words4::setPlant1(string value){
+void Words5::setPlant1(string value){
 	plant1 = value;
 }
 
-void Words4::setStory4(string value) {
-	story4 = value;
+void Words5::setStory5(string value) {
+	story5 = value;
 }
 
 /*string adjective9;
@@ -102,70 +102,70 @@ string state2;
 string plant1;
 string story4;*/
 
-string Words4::getAdjective9() {
+string Words5::getAdjective9() {
 	return adjective9;
 }
 
-string Words4::getAdjective10() {
+string Words5::getAdjective10() {
 	return adjective10;
 }
 
-string Words4::getAdjective11() {
+string Words5::getAdjective11() {
 	return adjective11;
 }
 
-string Words4::getPluralNoun10() {
+string Words5::getPluralNoun10() {
 	return pluralNoun10;
 }
 
-string Words4::getPluralNoun11() {
+string Words5::getPluralNoun11() {
 	return noun11;
 }
 
-string Words4::getPluralNoun12() {
+string Words5::getPluralNoun12() {
 	return pluralNoun12;
 }
 
-string Words4::getNoun8() {
+string Words5::getNoun8() {
 	return noun8;
 }
 
-string Words4::getNoun9() {
+string Words5::getNoun9() {
 	return noun9;
 }
 
-string Words4::getNoun10(){
+string Words5::getNoun10(){
 	return noun10;
 }
 
-string Words4::getNoun11(){
+string Words5::getNoun11(){
 	return noun11;
 }
 
-string Words4::getName1(){
+string Words5::getName1(){
 	return name1;
 }
 
-string Words4::getName2(){
+string Words5::getName2(){
 	return name2;
 }
 
-string Words4::getName3(){
+string Words5::getName3(){
 	return name3;
 }
 
-string Words4::getState1(){
+string Words5::getState1(){
 	return state1;
 }
 
-string Words4::getState2(){
+string Words5::getState2(){
 	return state2;
 }
 
-string Words4::getPlant1(){
+string Words5::getPlant1(){
 	return plant1;
 }
 
-string Words4::getStory4(){
-	return story4;
+string Words5::getStory5(){
+	return story5;
 }

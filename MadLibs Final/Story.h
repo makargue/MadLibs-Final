1a922096d169a3d22895a20eada4e4873694f182
makargue <mnargue02>
2016-05-03 08:31:42 -0400

    end of the day

--- MadLibs Final/Story.h
+++ MadLibs Final/Story.h
@@ -18,13 +18,13 @@ class StoryOne : public Story {
 public:
 	void makeStory(Words words) {
 		cout << " Thank You! Thank you from the bottom of my " << words.partOfBod1 << endl;
-		cout << " I don't know what to say. I'm speechless. I truly didn't expect to win this "<< words.adjective1 << endl;
-		cout << " certainly not with so many _" << words.adjective2<< endl;
+		cout << " I don't know what to say. I'm speechless. I truly didn't expect to win this " << words.adjective1 << endl;
+		cout << " certainly not with so many _" << words.adjective2 << endl;
 		cout << " actors competing in the same category.First and foremost, my thanks to " << words.femaleName1<< endl;
 		cout << " You couldn't work with a better " << words.occupation1 << endl;
 		cout << " And I'm sure I wouldn't be " << words.verbING1 << endl;
 		cout << " here tonight if it weren't for my " << words.occupation2 << endl;
-		cout << " director. I must also thank" << words.maleName1<< endl;
+		cout << " director. I must also thank" << words.maleName1 << endl;
 		cout << " who wrote a" << setw(11) << left << "." << words.adjective3<< endl;
 		cout << "script for me. Of course, none of this would be happening if it weren't for my agent, who convinced the network that I could play a 75 year - old, retired" << words.noun1 << endl;
 
@@ -34,16 +34,16 @@ public:
 class StoryTwo :public Story{
 public:
 	void makeStory(Words words){
-		cout << "I love to " << setw(11) << left << "." << setw(11) << right << it->getVerb1()->Words2 << endl;
-		cout << "video games. I can play them day and " << setw(11) << left << "!" << setw(11) << right << it->getNoun2()->Words2 << endl;
-		cout << "My mom and" << setw(11) << left << "!" << setw(11) << right << it->getNoun3()->Words2 << endl;
-		cout << "are not too happy with me" << setw(11) << left << "" << setw(11) << right << it->getVerbING2()->Words2 << endl;
-		cout << "so much time in front of the television" << setw(11) << right << it->getAdjective4()->Words2 << endl;
-		cout << "games help children develop hand-" << setw(11) << left << "." << setw(11) << right << it->getPartOfBod2()->Words2 << endl;
-		cout << "coordination and improve their learning" << setw(11) << left << "." << setw(11) << right << it->getPluralNoun1()->Words2 << endl;
-		cout << ", he also seems to think they have" << setw(11) << left << "." << setw(11) << right << it->getAdjective5()->Words2 << endl;
-		cout << "side effects on one's" << setw(11) << left << "." << setw(11) << right << it->getPartOfBod3()->Words2 << endl;
-		cout << "Both of my " << setw(11) << left << "." << setw(11) << right << it->getPluralNoun2()->Words2 << endl;
+		cout << "I love to " << words.verb1<< endl;
+		cout << "video games. I can play them day and " << words.noun2 << endl;
+		cout << "My mom and" << words.noun3<< endl;
+		cout << "are not too happy with me" << words.verbING2 << endl;
+		cout << "so much time in front of the television" << words.adjective4 << endl;
+		cout << "games help children develop hand-" << words.partOfBod2<< endl;
+		cout << "coordination and improve their learning" << words.pluralNoun1<< endl;
+		cout << ", he also seems to think they have" << words.adjective5<< endl;
+		cout << "side effects on one's"<< words.partOfBod3 << endl;
+		cout << "Both of my " << words.pluralNoun2<< endl;
 		cout << "think this is due to a " << setw(11) << left << "." << setw(11) << right << it->getAdjective6()->Words2 << endl;
 		cout << "use of violence in the majority of the " << setw(11) << left << "." << setw(11) << right << it->getPluralNoun3()->Words2 << endl;
 		cout << "Finally, we all arrived at a" << setw(11) << left << "!" << setw(11) << right << it->getNoun4()->Words2 << endl;
--- MadLibs Final/Words.h
+++ MadLibs Final/Words.h
@@ -7,4 +7,5 @@ using namespace std;
 class Words {
 public:
 	string noun1, adjective1, adjective2, adjective3, femaleName1, maleName1, occupation1, occupation2, partOfBod1, verbING1;
+
 };

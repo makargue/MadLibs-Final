6511cee527fc181aa6613c72215ee123e43446c1
makargue <mnargue02>
2016-05-03 08:08:37 -0400

    Class storytwo

--- MadLibs Final/Story.h
+++ MadLibs Final/Story.h
@@ -29,4 +29,26 @@ public:
 		cout << "script for me. Of course, none of this would be happening if it weren't for my agent, who convinced the network that I could play a 75 year - old, retired" << words.noun1 << endl;
 
 	}
+}; 
+
+class StoryTwo :public Story{
+public:
+	void makeStory(Words words){
+		cout << "I love to " << setw(11) << left << "." << setw(11) << right << it->getVerb1()->Words2 << endl;
+		cout << "video games. I can play them day and " << setw(11) << left << "!" << setw(11) << right << it->getNoun2()->Words2 << endl;
+		cout << "My mom and" << setw(11) << left << "!" << setw(11) << right << it->getNoun3()->Words2 << endl;
+		cout << "are not too happy with me" << setw(11) << left << "" << setw(11) << right << it->getVerbING2()->Words2 << endl;
+		cout << "so much time in front of the television" << setw(11) << right << it->getAdjective4()->Words2 << endl;
+		cout << "games help children develop hand-" << setw(11) << left << "." << setw(11) << right << it->getPartOfBod2()->Words2 << endl;
+		cout << "coordination and improve their learning" << setw(11) << left << "." << setw(11) << right << it->getPluralNoun1()->Words2 << endl;
+		cout << ", he also seems to think they have" << setw(11) << left << "." << setw(11) << right << it->getAdjective5()->Words2 << endl;
+		cout << "side effects on one's" << setw(11) << left << "." << setw(11) << right << it->getPartOfBod3()->Words2 << endl;
+		cout << "Both of my " << setw(11) << left << "." << setw(11) << right << it->getPluralNoun2()->Words2 << endl;
+		cout << "think this is due to a " << setw(11) << left << "." << setw(11) << right << it->getAdjective6()->Words2 << endl;
+		cout << "use of violence in the majority of the " << setw(11) << left << "." << setw(11) << right << it->getPluralNoun3()->Words2 << endl;
+		cout << "Finally, we all arrived at a" << setw(11) << left << "!" << setw(11) << right << it->getNoun4()->Words2 << endl;
+		cout << "compromise: After dinner I can play" << setw(11) << left << "!" << setw(11) << right << it->getNumber1()->Words2 << endl;
+		cout << "hours of video games, provided I help clear the" << setw(11) << left << "!" << setw(11) << right << it->getNoun5()->Words2 << endl;
+		cout << "and wash the" << setw(11) << left << " " << setw(11) << right << it->getPluralNoun4()->Words2 << endl;
+	}
 };

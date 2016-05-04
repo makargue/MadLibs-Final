253382bc63feaf6bbee8edc643e1a06b27f8cdeb
makargue <mnargue02>
2016-05-04 09:39:19 -0400

    string for story2

--- MadLibs Final/Words.h
+++ MadLibs Final/Words.h
@@ -7,5 +7,5 @@ using namespace std;
 class Words {
 public:
 	string noun1, adjective1, adjective2, adjective3, femaleName1, maleName1, occupation1, occupation2, partOfBod1, verbING1;
-
+	string adjective4, adjective5, adjective6, adjective7, noun2, noun3, noun4, noun5, number1, partOfBod2, partOfBod3, pluralNoun1, pluralNoun2, pluralNoun3, pluralNoun4, verb1, verbING2;
 };

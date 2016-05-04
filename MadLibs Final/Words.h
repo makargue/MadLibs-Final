0a5bd8051f26aab9bc75bf1091b5465908ef1a05
makargue <mnargue02>
2016-05-04 09:44:30 -0400

    string story3

--- MadLibs Final/Words.h
+++ MadLibs Final/Words.h
@@ -8,4 +8,6 @@ class Words {
 public:
 	string noun1, adjective1, adjective2, adjective3, femaleName1, maleName1, occupation1, occupation2, partOfBod1, verbING1;
 	string adjective4, adjective5, adjective6, adjective7, noun2, noun3, noun4, noun5, number1, partOfBod2, partOfBod3, pluralNoun1, pluralNoun2, pluralNoun3, pluralNoun4, verb1, verbING2;
+	string adjective8, celebrity1, lastName1, lastName2, noun6, noun7, pluralAnimal1, pluralNoun5, pluralNoun6, pluralNoun7, pluralNOun8, pluralNoun9;
+
 };

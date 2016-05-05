68d2022bc1dcc45a4b58ec7e9a5cafd14f3894ca
makargue <mnargue02>
2016-05-05 07:37:49 -0400

    fixed error

--- MadLibs Final/Words.h
+++ MadLibs Final/Words.h
@@ -8,7 +8,7 @@ class Words {
 public:
 	string noun1, adjective1, adjective2, adjective3, femaleName1, maleName1, occupation1, occupation2, partOfBod1, verbING1;
 	string adjective4, adjective5, adjective6, adjective7, noun2, noun3, noun4, noun5, number1, partOfBod2, partOfBod3, pluralNoun1, pluralNoun2, pluralNoun3, pluralNoun4, verb1, verbING2;
-	string adjective8, celebrity1, lastName1, lastName2, noun6, noun7, pluralAnimal1, pluralNoun5, pluralNoun6, pluralNoun7, pluralNOun8, pluralNoun9;
+	string adjective8, celebrity1, lastName1, lastName2, noun6, noun7, pluralAnimal1, pluralNoun5, pluralNoun6, pluralNoun7, pluralNoun8, pluralNoun9;
 	string adjective9, adjective10, adjective11, pluralNoun10, pluralNoun11, noun8, noun9, noun10, name1, name2, state1, state2, plant1;
 	string noun11, noun12, noun13, noun14, noun15, adjective12, adjective13, verb2, verbING3, name4, animal1, adverb1, feeling1, conjunction1;
 	string adjective14, adjective15, adjective16, adjective17, adjective18, adjective19, adjective20, adverb2, noun15, noun16, noun17, number2, name5, name6, name7, pluralNoun12, pluralNoun13, pluralNoun14, pluralNoun15, verb3, partOfBod4, liquid1;

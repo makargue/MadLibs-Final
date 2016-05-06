25c01a9c40173ae576a7edb48a2c9bf556f5d727
makargue <mnargue02>
2016-05-06 08:01:32 -0400

    words 3 main.cpp created and outline done

--- MadLibs Final/Main.cpp
+++ MadLibs Final/Main.cpp
@@ -118,6 +118,47 @@ int main(Words2){
 	cin >> words.verbING2;
 
 
+	StoryTwo two;
+	two.makeStory(words);
+
+}
+int main(Words3){
+	string adjective8, celebrity1, lastName1, lastName2, noun6, noun7, pluralAnimal1, pluralNoun5, pluralNoun6, pluralNoun7, pluralNoun8, pluralNoun9;
+	noun1 = noun1;
+	adjective1 = adjective1;
+	adjective2 = adjective2;
+	adjective3 = adjective3;
+	femaleName1 = femaleName1;
+	maleName1 = maleName1;
+	occupation1 = occupation1;
+	occupation2 = occupation2;
+	partOfBod1 = partOfBod1;
+
+	Words words;
+
+
+	cout << "Enter a noun:";
+	cin >> words.noun1;
+	cout << "Enter an adjective:";
+	cin >> words.adjective1;
+	cout << "Enter an adjective:";
+	cin >> words.adjective2;
+	cout << "Enter an adjective";
+	cin >> words.adjective3;
+	cout << "Enter a name:";
+	cin >> words.femaleName1;
+	cout << "Enter a name:";
+	cin >> words.maleName1;
+	cout << "Enter a job:";
+	cin >> words.occupation1;
+	cout << "Enter a job:";
+	cin >> words.occupation2;
+	cout << "Enter a body part:";
+	cin >> words.partOfBod1;
+	cout << "Enter a verb ending in ING";
+	cin >> words.verbING1;
+
+
 	StoryOne one;
 	one.makeStory(words);
 

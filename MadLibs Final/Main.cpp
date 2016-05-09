2b1e85e63bd40a8263ea68121a5d110c184d1e9e
makargue <mnargue02>
2016-05-09 07:52:33 -0400

    words4 main.cpp start, string finished

--- MadLibs Final/Main.cpp
+++ MadLibs Final/Main.cpp
@@ -122,6 +122,7 @@ int main(Words2){
 	two.makeStory(words);
 
 }
+
 int main(Words3){
 	string adjective8, celebrity1, lastName1, lastName2, noun6, noun7, pluralAnimal1, pluralNoun5, pluralNoun6, pluralNoun7, pluralNoun8, pluralNoun9;
 	adjective8 = adjective8;
@@ -171,6 +172,52 @@ int main(Words3){
 
 }
 
+int main(Words3){
+	string adjective9, adjective10, adjective11, pluralNoun10, pluralnoun11, noun8, noun9, noun10, name1, name2, name3, state1, state2, plant1;
+	adjective8 = adjective8;
+	celebrity1 = celebrity1;
+	lastName1 = lastName1;
+	lastName2 = lastName2;
+	noun6 = noun6;
+	noun7 = noun7;
+	pluralAnimal1 = pluralAnimal1;
+	pluralNoun5 = pluralNoun5;
+	pluralNoun6 = pluralNoun6;
+	pluralNoun7 = pluralNoun7;
+	pluralNoun8 = pluralNoun8;
+	pluralNoun9 = pluralNoun9;
+
+	Words words;
+
+
+	cout << "Enter an adjective:";
+	cin >> words.adjective8;
+	cout << "Enter a celebrity:";
+	cin >> words.celebrity1;
+	cout << "Enter a last name:";
+	cin >> words.lastName1;
+	cout << "Enter a last name";
+	cin >> words.lastName2;
+	cout << "Enter a noun:";
+	cin >> words.noun6;
+	cout << "Enter a noun:";
+	cin >> words.noun7;
+	cout << "Enter a plural animal";
+	cin >> words.pluralAnimal1;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun5;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun6;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun7;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun8;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun9;
+
+
+	StoryThree three;
+	three.makeStory(words);
 	/*STORY ONE
 	cout << " Thank You! Thank you from the bottom of my " << getPartOfBod1() << endl;
 	cout << " I don't know what to say. I'm speechless. I truly didn't expect to win this " getAdjective1() << endl;

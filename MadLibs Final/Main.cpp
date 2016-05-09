ba4ec37f7ea616ebc67b214228ea2c865e868c0b
makargue <mnargue02>
2016-05-09 08:11:53 -0400

    outline made for words 5, 6, and 7 in main.cpp

--- MadLibs Final/Main.cpp
+++ MadLibs Final/Main.cpp
@@ -225,6 +225,174 @@ int main(Words4){
 
 	StoryFour four;
 	four.makeStory(words);
+}
+int main(Words5){
+	string adjective9, adjective10, adjective11, pluralNoun10, pluralnoun11, noun8, noun9, noun10, name1, name2, name3, state1, state2, plant1;
+	adjective9 = adjective9;
+	adjective10 = adjective10;
+	adjective11 = adjective11;
+	pluralNoun10 = pluralNoun10;
+	pluralnoun11 = pluralnoun11;
+	noun8 = noun8;
+	noun9 = noun9;
+	noun10 = noun10;
+	name1 = name1;
+	name2 = name2;
+	name3 = name3;
+	state1 = state1;
+	state2 = state2;
+	plant1 = plant1;
+
+	Words words;
+
+
+	cout << "Enter an adjective:";
+	cin >> words.adjective9;
+	cout << "Enter an adjective:";
+	cin >> words.adjective10;
+	cout << "Enter an adjective:";
+	cin >> words.adjective11;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun9;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun10;
+	cout << "Enter a plural noun";
+	cin >> words.pluralNoun11;
+	cout << "Enter a noun:";
+	cin >> words.noun8;
+	cout << "Enter a noun:";
+	cin >> words.noun9;
+	cout << "Enter a noun";
+	cin >> words.noun10;
+	cout << "Enter a name:";
+	cin >> words.name1;
+	cout << "Enter a name:";
+	cin >> words.name2;
+	cout << "Enter a name:";
+	cin >> words.name3;
+	cout << "Enter a state:";
+	cin >> words.state1;
+	cout << "Enter a state:";
+	cin >> words.state2;
+	cout << "Enter a plant:";
+
+
+	StoryFive five;
+	five.makeStory(words);
+}
+
+int main(Words6){
+	string adjective9, adjective10, adjective11, pluralNoun10, pluralnoun11, noun8, noun9, noun10, name1, name2, name3, state1, state2, plant1;
+	adjective9 = adjective9;
+	adjective10 = adjective10;
+	adjective11 = adjective11;
+	pluralNoun10 = pluralNoun10;
+	pluralnoun11 = pluralnoun11;
+	noun8 = noun8;
+	noun9 = noun9;
+	noun10 = noun10;
+	name1 = name1;
+	name2 = name2;
+	name3 = name3;
+	state1 = state1;
+	state2 = state2;
+	plant1 = plant1;
+
+	Words words;
+
+
+	cout << "Enter an adjective:";
+	cin >> words.adjective9;
+	cout << "Enter an adjective:";
+	cin >> words.adjective10;
+	cout << "Enter an adjective:";
+	cin >> words.adjective11;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun9;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun10;
+	cout << "Enter a plural noun";
+	cin >> words.pluralNoun11;
+	cout << "Enter a noun:";
+	cin >> words.noun8;
+	cout << "Enter a noun:";
+	cin >> words.noun9;
+	cout << "Enter a noun";
+	cin >> words.noun10;
+	cout << "Enter a name:";
+	cin >> words.name1;
+	cout << "Enter a name:";
+	cin >> words.name2;
+	cout << "Enter a name:";
+	cin >> words.name3;
+	cout << "Enter a state:";
+	cin >> words.state1;
+	cout << "Enter a state:";
+	cin >> words.state2;
+	cout << "Enter a plant:";
+
+
+	StorySix six;
+	six.makeStory(words);
+}
+int main(Words7){
+	string adjective9, adjective10, adjective11, pluralNoun10, pluralnoun11, noun8, noun9, noun10, name1, name2, name3, state1, state2, plant1;
+	adjective9 = adjective9;
+	adjective10 = adjective10;
+	adjective11 = adjective11;
+	pluralNoun10 = pluralNoun10;
+	pluralnoun11 = pluralnoun11;
+	noun8 = noun8;
+	noun9 = noun9;
+	noun10 = noun10;
+	name1 = name1;
+	name2 = name2;
+	name3 = name3;
+	state1 = state1;
+	state2 = state2;
+	plant1 = plant1;
+
+	Words words;
+
+
+	cout << "Enter an adjective:";
+	cin >> words.adjective9;
+	cout << "Enter an adjective:";
+	cin >> words.adjective10;
+	cout << "Enter an adjective:";
+	cin >> words.adjective11;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun9;
+	cout << "Enter a plural noun:";
+	cin >> words.pluralNoun10;
+	cout << "Enter a plural noun";
+	cin >> words.pluralNoun11;
+	cout << "Enter a noun:";
+	cin >> words.noun8;
+	cout << "Enter a noun:";
+	cin >> words.noun9;
+	cout << "Enter a noun";
+	cin >> words.noun10;
+	cout << "Enter a name:";
+	cin >> words.name1;
+	cout << "Enter a name:";
+	cin >> words.name2;
+	cout << "Enter a name:";
+	cin >> words.name3;
+	cout << "Enter a state:";
+	cin >> words.state1;
+	cout << "Enter a state:";
+	cin >> words.state2;
+	cout << "Enter a plant:";
+
+
+	StorySeven seven;
+	seven.makeStory(words);
+}
+
+
+
+
 	/*STORY ONE
 	cout << " Thank You! Thank you from the bottom of my " << getPartOfBod1() << endl;
 	cout << " I don't know what to say. I'm speechless. I truly didn't expect to win this " getAdjective1() << endl;

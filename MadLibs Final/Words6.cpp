f6fd30ae1e231f669039943593c5b28f170c0965
makargue <mnargue02>
2016-05-12 08:24:00 -0400

    words 6 and 7

---
+++ MadLibs Final/Words6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include "Words.h"
+#include "Story6.cpp"
+#include "Story.h"
+
+using namespace std;
---
+++ MadLibs Final/Words7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include "Words.h"
+#include "Story7.cpp"
+#include "Story.h"
+
+using namespace std;
